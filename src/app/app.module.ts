4f3748f7191e52c58212fa50159b14af63329693
AnSa <mauroscout@hotmail.com>
2022-02-06 05:54:36 -0500

    level Model
    Implement search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BackEndModule } from './back-end/back-end.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BackEndModule
   ],
   providers: [],
   bootstrap: [AppComponent]
